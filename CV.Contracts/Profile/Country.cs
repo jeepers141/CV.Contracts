164424fc53ee3fd4800895cbc4d1450b0211d4d1
Gareth Knight <gareth.knight@outlook.com>
2018-10-08 12:32:34 +0200

    Fixed string interpolation on ApiUrl

--- CV.Contracts/Profile/Country.cs
+++ CV.Contracts/Profile/Country.cs
@@ -3,7 +3,7 @@ namespace CV.Contracts.Profile
     public class Country : ICountry
     {
         public string Name { get; set; }
-        public string ApiUrl => "https://www.countryflags.io/{Code}/"; 
+        public string ApiUrl => $"https://www.countryflags.io/{Code}/"; 
         public string Code { private get; set; }
     }
 }
